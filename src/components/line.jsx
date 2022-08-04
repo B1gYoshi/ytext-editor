396e03bb55914117378a47ad21377bc5bf308196
Neel Joshi <neelsaliljoshi@gmail.com>
2022-08-04 13:07:48 -0400

    Comments

--- src/components/line.jsx
+++ src/components/line.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Badge, Button } from 'react-bootstrap';
+import { Badge } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './fonts.css';
 import './lines.css';
 
+//The Line class holds the badge representing the line number and the line itself
 class Line extends Component {
 
+    //the styling for the line 
     lineStyle = {
         fontFamily: "Gentium Book Plus",
         color:'white',
@@ -14,48 +16,58 @@ class Line extends Component {
         width: '98%',
     }
 
-    buttonStyle = {
+    //the styling for the badge
+    badgeStyle = {
         width: '2%',
     }        
 
+    //called when any key on the keyboard is pressed
+    //param: [e] represents the event which holds information about the key pressed
     handleKeyDown = (e) => {
-        if (e.key === "Enter") {
+        if (e.key === "Enter") { //create new line
             this.props.newLine(this.props.lnum, this.line.selectionStart)
-        } else if (e.key === "ArrowUp") {
+        } else if (e.key === "ArrowUp") { //move up a line
             this.props.upLine(this.props.curCursor)
-        } else if (e.key === "ArrowDown") {
+        } else if (e.key === "ArrowDown") { //move down a line
             this.props.downLine(this.props.curCursor)
-        } else if (e.key === "Tab") {
+        } else if (e.key === "Tab") { //tab = four spaces
             e.preventDefault()
             let start = this.line.selectionStart
             this.props.tab(this.props.lnum, start)
             setTimeout(() => this.line.setSelectionRange(start +4, start+4), 0)
-        } else if (e.key === "Backspace" && this.line.selectionStart === 0 && this.props.lnum!== 0) {
+        } else if (e.key === "Backspace" && this.line.selectionStart === 0 && this.props.lnum!== 0) { //delete a line
             this.props.deleteLine(this.props.lnum)
         }
     }
 
+    //called every time a componenet is mounted
     componentDidMount() {
+        //checks if the cursor should be on this specific line
         if (this.props.curCursor === this.props.lnum) {
             this.line.focus();
         }
     }
 
+    //called every time a component is updated
     componentDidUpdate () {
+        //checks if the cursor should be on this specific line
         if (this.props.curCursor === this.props.lnum) {
             this.line.focus();
         }  
     }
 
+    //called every time the line is focused
     handleFocus = () => {
+        //this sends the caret to the end of the line
         setTimeout(() => this.line.setSelectionRange(this.line.value.length, this.line.value.length), 0) //Apparently not good practice, find a better sol
     }
 
+    //returns a div with the badge containing the line number and the line 
     render() { 
         return( 
             <div>
-                <Badge bg='dark' style={this.buttonStyle} >{this.props.lnum+1}</Badge>
-                <input className='line' onFocus={this.handleFocus} onMouseDown={() => this.props.mouseDown(this.props.lnum)} onMouseUp={() => this.props.mouseUp(this.props.lnum)} ref={(input) => { this.line = input }} style={this.lineStyle} value={this.props.content} onKeyDown={this.handleKeyDown} onChange={(e) => this.props.change(e, this.props.lnum)} />
+                <Badge bg='dark' style={this.badgeStyle} >{this.props.lnum+1}</Badge>
+                <input onFocus={this.handleFocus} onMouseDown={() => this.props.mouseDown(this.props.lnum)} onMouseUp={() => this.props.mouseUp(this.props.lnum)} ref={(input) => { this.line = input }} style={this.lineStyle} value={this.props.content} onKeyDown={this.handleKeyDown} onChange={(e) => this.props.change(e, this.props.lnum)} />
             </div>
         );
     }
--- src/components/lines.jsx
+++ src/components/lines.jsx
@@ -7,13 +7,16 @@ import ryoshi from './ryoshi.png'
 import wow from './yoshi-wooh.mp3'
 import wah from './yoshi-waaah.mp3'
 
+//The Lines class holds the header, images, and all the present lines in the code editor
 class Lines extends Component {
     
     state = { 
-        cursor: 0,
-        lines: ["\n"],
-        mouseDown: false
+        cursor: 0, //represents which line the cursor/caret should be on 
+        lines: [""], //each element in this array represents the †ext-content of a line 
+        mouseDown: false //checks if the mouse is down (user has clicked)
     } 
+
+    //styling for both images of yoshi
 
     lImgStyle = {
         width: '2%',
@@ -27,6 +30,7 @@ class Lines extends Component {
         float: 'right'
     }
 
+    //styling for the header between the images
     headerStyle = {
         height:'30px',
         width:'96%',
@@ -34,31 +38,44 @@ class Lines extends Component {
         display: 'inline-block'
     }
 
+    //plays an audio clip of yoshi saying wow
     playWow = () => {
         new Audio(wow).play();
     }
 
+    //plays an audio clip of yoshi saying wah
     playWah = () => {
         new Audio(wah).play();
     }
 
+    //handles creation of a new line
+    //param: [index] represents the line number where the new line is created
+    //param: [caretPos] represents the position of the caret within the line when enter is hit
+        //e.g. if `Hello World` was the line and caretPos was 6, then `World` would go on the new line
     handleNewLine = (index, caretPos) => {
-        this.playWow();
+        this.playWow(); //when a new line is created, the wow audio clip is played
+
         let newLines = [...this.state.lines]
         newLines.splice(index+1, 0, "")
-        const newCursor = this.state.cursor+1
+
         let originalLine = this.state.lines[index]
+        
         newLines[index] = originalLine.substring(0,caretPos)
         newLines[index+1] = originalLine.substring(caretPos, originalLine.length)
+
+        const newCursor = this.state.cursor+1
+
         this.setState({lines:newLines, cursor: newCursor})
     }
 
+    //handles any change to a line
+    //param: [e] represents the event containing information about the change
+    //param: [index] represents the line number where the change occurred
     handleChange = (e, index) => {
 
         const change = e.target.value
         var updatedLines = [...this.state.lines]
 
-        //roundabout solution to the double space -> period problem for mac honestly but i don't know/couldn't find a better solution 
         if (change.length > this.state.lines[index].length && change.substring(change.length-1, change.length) === " ") {
             updatedLines[index] += " "
         } else {
@@ -68,6 +85,8 @@ class Lines extends Component {
         this.setState({lines: updatedLines})
     }
 
+    //handles going up a line
+    //param: [index] represents the current line number 
     handleUp = (index) => {
         if (index <= 0) return;
         const newCursor = this.state.cursor -1;
@@ -75,20 +94,29 @@ class Lines extends Component {
 
     }
 
+    //handles going down a line
+    //param: [index] represents the current line number
     handleDown = (index) => {
         if (index >= this.state.lines.length - 1) return;
         const currentCursor = this.state.cursor + 1;
         this.setState({cursor:currentCursor})
     }
     
+    //handles mouse clicks
+    //*param: [index] represents the line number that the mouse clicked
+        //this is different from the other index parameters
     handleMouseDown = (index) => {
         this.setState({cursor: index, mouseDown:true})
     }
 
-    handleMouseUp = (index) => {
+    //handles when mouse click is released
+    handleMouseUp = () => {
         this.setState({mouseDown: false}) 
     }
 
+    //handles tab click
+    //param: [index] represents current line number
+    //param: [caretPos] represents position of the caret within the line
     handleTab = (index, caretPos) => {
         var updatedLines = [...this.state.lines]
         let originalLine = updatedLines[index]
@@ -96,18 +124,23 @@ class Lines extends Component {
         this.setState({lines: updatedLines})
     }
 
+    //handles deletion of a line
+    //param: [index] represents line to be deleted
     handleDeleteLine = (index) => {
-        this.playWah()
+        this.playWah() //plays wah audio clip when a line is deleted
+
         const deletedLine = this.state.lines[index]
         var updatedLines = [...this.state.lines]
+
         updatedLines.splice(index, 1)
         updatedLines[index-1] += deletedLine + " "
-        this.setState({lines:updatedLines, cursor:index-1})
-        
+
+        this.setState({lines:updatedLines, cursor:index-1}) 
     }
 
+    //renders images, header, and all the lines
     render() { 
-        let keyCount = 0;
+        let keyCount = 0; //used for assigning line number
         return (
             <div className='bg'>
                 <img alt="Cannot Display" style={this.lImgStyle} src={lyoshi}/>
